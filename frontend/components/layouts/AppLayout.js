f458127c7efdf182cfd74fd13c011e4439ff7224
mango <mangogoon@gmail.com>
2022-03-13 20:22:20 +0900

    기본 화면 구조 설정

---
+++ frontend/components/layouts/AppLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AppLayout = () => {
+  return (
+    <>
+      <ul>
+        <li>메뉴1</li>
+        <li>메뉴2</li>
+      </ul>
+    </>
+  );
+};
+
+export default AppLayout;
